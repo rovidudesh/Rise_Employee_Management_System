1d4da613b39077999b3af470b119ff85f4a0898b
yasiith <yasithvimukthi259@gmail.com>
2025-07-14 19:15:32 +0530

    Dark mode added to employee task history table

--- app/templates/frontend/app/employee/history/page.tsx
+++ app/templates/frontend/app/employee/history/page.tsx
@@ -1,20 +1,23 @@
-'use client'
-import React from 'react'
-import Navbar from '../../components/employee/Navbar'
-import Footer from '../../components/Footer'
-import HistoryTable from '../../components/employee/HistoryTable'
+"use client";
+import React from "react";
+import Navbar from "../../components/employee/Navbar";
+import Footer from "../../components/Footer";
+import HistoryTable from "../../components/employee/HistoryTable";
 
-const page = () => {
+const HistoryPage = () => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className="flex flex-col min-h-screen bg-gray-100 dark:bg-slate-900 transition-colors duration-300">
       <Navbar />
-      <main className="flex-grow">
-        <HistoryTable />
+
+      <main className="flex-grow bg-gray-50 dark:bg-slate-800 transition-colors duration-300">
+        <div className="container mx-auto px-2 sm:px-4 py-4 sm:py-8">
+          <HistoryTable />
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default HistoryPage;
